1370943f91877aed267ba7f22546278ff5e9017e
eran <eran.lerer@gmail.com>
2018-06-06 11:59:45 +0300

    har/load mode

--- har_executer.go
+++ har_executer.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"github.com/elerer/hargo"
-	"io/ioutil"
 	"bufio"
 	"bytes"
+	"flag"
+	"github.com/elerer/hargo"
+	"io/ioutil"
 	"net/url"
+	"strings"
+	"fmt"
 )
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -14,11 +18,17 @@ func check(e error) {
 }
 
 func main() {
-	dat, err := ioutil.ReadFile("/home/elerer/har/dis.har")
+	modePtr := flag.String("mode", "har", "har - follows har file and validate | load - perform load test")
+	filePtr := flag.String("hf", "/home/elerer/har/dis.har", "path to har file")
+	flag.Parse()
+
+	isHar := strings.Compare(*modePtr, "har") == 0
+	dat, err := ioutil.ReadFile(*filePtr)
 	check(err)
+	fmt.Printf("run %s test , har file %s\n",*modePtr,*filePtr)
 	//fmt.Print(string(dat))
 	br := bytes.NewReader(dat)
 	r := bufio.NewReader(br)
-	infurl,_ := url.Parse("http://127.0.0.1:8086")
-	hargo.LoadTest("/home/elerer/har/dis.har",r,1,600000000000,*infurl,false)
+	infurl, _ := url.Parse("http://127.0.0.1:8086")
+	hargo.LoadTest("/home/elerer/har/dis.har", r, 1, 600000000000, *infurl, false,isHar)
 }
